ff23b0153667a7ebb4977dea64c8643c1a10e44a
prachi2829 <prachiyadav419@gmail.com>
2026-04-05 23:39:48 +0530

    downgrade express to v4

--- server/server.js
+++ server/server.js
@@ -22,8 +22,8 @@ const path = require("path");
 __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client","build")));
-  app.get("/(.*)", (req, res) => {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
